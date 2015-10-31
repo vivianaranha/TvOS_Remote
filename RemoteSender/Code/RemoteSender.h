75e154a7df78ae27df0c73ddf7682bea761affc8
Vivian Aranha <vivianaranha@gmail.com>
2015-10-31 15:25:39 -0400

    Adding Code files

---
+++ RemoteSender/Code/RemoteSender.h
@@ -0,0 +1,15 @@
+//
+//  RemoteSender.h
+//  RemoteSender
+//
+//  Created by Vivian Aranha on 9/22/15.
+//  Copyright © 2015 Vivian Aranha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RemoteSender : NSObject
+
+-(void)sendInfo:(NSDictionary *)infoDict;
+
+@end
